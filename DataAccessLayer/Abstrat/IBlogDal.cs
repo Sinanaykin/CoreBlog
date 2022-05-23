11515d4c7106b94bbbadb71edcd3f7f75cb59501
Sinan Aykin <sinanaykinnn@gmail.com>
2022-05-23 17:25:20 +0300

    Generic yapı oluşturulmaya başlandı

---
+++ DataAccessLayer/Abstrat/IBlogDal.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstrat
+{
+    interface IBlogDal
+    {
+        List<Blog> ListAllBlog();
+        void BlogAdd(Blog blog);
+        void DeleteBlog(Blog blog);
+        void UpdateBlog(Blog blog);
+        Blog GetById(int id);
+    }
+}
---
+++ DataAccessLayer/Repositories/CategoryRepository.cs
@@ -0,0 +1,43 @@
+﻿using DataAccessLayer.Abstrat;
+using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class CategoryRepository : ICategoryDal
+    {
+        Context c = new Context(); //Böyle yazarsak aşğıda hepsinde kullanabiliriz
+        public void CategoryAdd(Category category)
+        {
+            c.Add(category);
+            c.SaveChanges();
+        }
+
+        public void DeleteCategory(Category category)
+        {
+            c.Remove(category);
+            c.SaveChanges();
+        }
+
+        public Category GetById(int id)
+        {
+            return c.Categories.Find(id);
+        }
+
+        public List<Category> ListAllCategory()
+        {
+            return c.Categories.ToList();
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            c.Update(category);
+            c.SaveChanges();
+        }
+    }
+}
