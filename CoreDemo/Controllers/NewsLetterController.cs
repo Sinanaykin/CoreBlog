5fe0c91614b900cbc70a138c9cf91a0b3278cf4f
Sinan Aykin <sinanaykinnn@gmail.com>
2022-07-31 22:42:29 +0300

    Mail Bültenine abone olma  ve Yorum yapma kısımları eklendi

---
+++ CoreDemo/Controllers/NewsLetterController.cs
@@ -0,0 +1,29 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Controllers
+{
+    public class NewsLetterController : Controller
+    {
+        NewsLetterManager nm = new NewsLetterManager(new EfNewsLetterRepository());
+
+        [HttpGet]
+        public PartialViewResult SubscribeMail()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public PartialViewResult SubscribeMail(NewsLetter p)//Bunları Partial yapıyoruz çünkü başka bir view içinde kullanıyoruz  bunu
+        {
+            p.MailStatus = true;
+            nm.AddNewsLetter(p);
+            return PartialView();
+        }
+    }
+}
