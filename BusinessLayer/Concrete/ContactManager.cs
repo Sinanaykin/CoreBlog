28b983c03bbf7d8f6c3b612778f06cf0ab718b4a
Sinan Aykin <sinanaykinnn@gmail.com>
2022-08-21 23:43:08 +0300

    İletişim sayfası oluşturuldu ve düzenlendi

---
+++ BusinessLayer/Concrete/ContactManager.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstrat;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class ContactManager : IContactService
+    {
+        IContactDal _contactdal;
+        public ContactManager(IContactDal contactdal)
+        {
+            _contactdal = contactdal;
+        }
+        public void ContactAdd(Contact contact)
+        {
+            _contactdal.Insert(contact);
+        }
+    }
+}
---
+++ CoreDemo/Controllers/ContactController.cs
@@ -0,0 +1,32 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Controllers
+{
+    public class ContactController : Controller
+    {
+        ContactManager cm = new ContactManager(new EfContactRepository());
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Contact p)
+        {
+            //Bazı alanları db den değilde burdan manuel aldık şimdilik
+            p.ContactDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            p.ContactStatus = true;
+            cm.ContactAdd(p);
+            return RedirectToAction("Index", "Blog");
+        }
+    }
+}
