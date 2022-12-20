f5741cfc41a10ba51f3472f8b5d12951d0fe4012
Sinan Aykin <sinanaykinnn@gmail.com>
2022-12-20 23:25:51 +0300

    Bildirim kısmı Componente taşındı ve bildirimler listelendi

--- DataAccessLayer/Migrations/20221220193530_mig_notification_add_table.Designer.cs
+++ DataAccessLayer/Migrations/20221220193530_mig_notification_add_table.Designer.cs
@@ -4,14 +4,16 @@ using DataAccessLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20221220193530_mig_notification_add_table")]
+    partial class mig_notification_add_table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -222,9 +224,6 @@ namespace DataAccessLayer.Migrations
                         .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
                     b.Property<string>("NotificaitonDetails")
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<string>("NotificationColor")
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<DateTime>("NotificationDate")
